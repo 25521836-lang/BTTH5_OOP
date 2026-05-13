ae8f47a56192bcb76d86bf01ef382c5efabd8392
25521836-lang <25521836@gm.uit.edu.vn>
2026-05-13 10:59:36 +0700

    Create GiaoDich.cpp

---
+++ Bai2/GiaoDich.cpp
@@ -0,0 +1,25 @@
+#include "GiaoDich.h"
+
+void GiaoDich::Nhap(){
+    cin.ignore();
+    cout<<"Nhap ma giao dich:";
+    getline(cin,maGD);
+    cout<<"Njap ngay giao dich:";
+    getline(cin,ngayGD);
+    cout<<"Nhap don gia:";
+    cin>>donGia;
+    cout<<"Nhap dien tich:";
+    cin>>dienTich;
+}
+void GiaoDich::Xuat(){
+    cout<<"Ma GD:"<<maGD<<endl;
+    cout<<"Ngay GD:"<<ngayGD<<endl;
+    cout<<"Don gia:"<<donGia<<endl;
+    cout<<"Dien tich:"<<dienTich<<endl;
+}
+string GiaoDich::getNgayGD(){
+    return ngayGD;
+}
+double GiaoDich::thanhTien(){
+    return 0;
+}
