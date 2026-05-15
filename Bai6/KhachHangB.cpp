20cca3a2bb31663146b26607dcbbd8bde6a20c82
25521836-lang <25521836@gm.uit.edu.vn>
2026-05-15 12:07:18 +0700

    Create KhachHangB.cpp

---
+++ Bai6/KhachHangB.cpp
@@ -0,0 +1,17 @@
+#include "KhachHangB.h"
+
+void KhachHangB::nhap(istream &in){
+    KhachHang::nhap(in);
+    cout << "Nhap so nam than thiet: ";
+    in >> soNamThanThiet;
+    in.ignore();
+}
+double KhachHangB::tinhTien(){
+    double khuyenMai = soNamThanThiet * 0.05;
+    if(khuyenMai > 0.5){
+        khuyenMai = 0.5;
+    }
+    double tien = soLuong * donGia;
+    tien = tien * (1 - khuyenMai);
+    return tien + tien * 0.1;
+}
