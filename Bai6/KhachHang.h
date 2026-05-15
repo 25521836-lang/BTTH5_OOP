29d63b845898437e5d3a08ae30cf41f3fe1cc1cc
25521836-lang <25521836@gm.uit.edu.vn>
2026-05-15 12:05:06 +0700

    Create KhachHang.h

---
+++ Bai6/KhachHang.h
@@ -0,0 +1,25 @@
+#ifndef KHACHHANG_H
+#define KHACHHANG_H
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class KhachHang{
+
+protected:
+    string ten;
+    int soLuong;
+    double donGia;
+
+public:
+
+    virtual void nhap(istream &in);
+
+    virtual double tinhTien() = 0;
+
+    string getTen();
+};
+
+#endif
