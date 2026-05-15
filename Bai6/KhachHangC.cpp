507b7338b8d953693c1fca4d2f3b267f894a03fe
25521836-lang <25521836@gm.uit.edu.vn>
2026-05-15 16:47:13 +0700

    Update KhachHangC.cpp

--- Bai6/KhachHangC.cpp
+++ Bai6/KhachHangC.cpp
@@ -1,8 +1,6 @@
 #include "KhachHangC.h"
 
-double KhachHangC::tinhTien(){
-
-    double tien = soLuong * donGia * 0.5;
-
-    return tien + tien * 0.1;
+double KhachHangC::tinhTien() {
+    double tien = soLuongHang * donGiaHang;
+    return tien * 0.5 + tien * 0.1;
 }
